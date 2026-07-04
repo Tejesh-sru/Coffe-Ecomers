28371cabe3797a244eb2b63daac6c3dbced7623d
Tejesh Jakkula <jakkulatejesh2005@gmail.com>
2026-07-04 07:08:53 +0530

    Fix formatting for minlength property in User model

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
       trim: true,
-      minlength: 1,
+      minlength: 1, 
     },
     email: {
       type: String,
